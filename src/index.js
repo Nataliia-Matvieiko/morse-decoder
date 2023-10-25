feb3329fb27f72b521c5c52502d2ac63a96e65f6
Natali Matvieiko <70928980+Nataliia-Matvieiko@users.noreply.github.com>
2023-10-25 17:42:33 +0200

    init: solve the task

--- src/index.js
+++ src/index.js
@@ -1,44 +1,76 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
+function decodeLetter(letter) {
+    if (letter === "**********") {
+        return " "
+    }
+
+    let morse = ""
+
+    for (let i = 0; i < letter.length; i += 2) {
+        const part = letter.substring(i, i + 2)
+        if (part === "10") {
+            morse += "."
+        } else if (part === "11") {
+            morse += "-";
+        }
+    }
+
+    return MORSE_TABLE[morse]
+}
+
 function decode(expr) {
-    // write your solution here
+    const letters = [];
+    while (expr.length > 0) {
+        const first = expr.substring(0, 10);
+        expr = expr.substring(10);
+        letters.push(first);
+    }
+
+    const decodedLetters = [];
+
+    for (let i = 0; i < letters.length; i++) {
+        decodedLetters.push(decodeLetter(letters[i]))
+    }
+
+    return decodedLetters.join("")
 }
 
 module.exports = {
